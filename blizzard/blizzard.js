af24b8f5a5019f06e2520f59e82cfe2a7e3905c5
WonJun-Yeo <rbgh004@gmail.com>
2021-11-29 18:05:28 +0900

    -

---
+++ blizzard/blizzard.js
@@ -0,0 +1,100 @@
+var nowficture = 1;
+
+$(".banner-after-bt").on("click", function () {
+  if (nowficture == 1) {
+    $(".banner-list").css("transform", "translateX(-20%)");
+    $(".banner-now-bt-list").css("background-color", "#fff");
+    $(".banner-now-bt-list").eq(1).css("background-color", "#006fb2");
+    nowficture = 2;
+  } else if (nowficture == 2) {
+    $(".banner-list").css("transform", "translateX(-40%)");
+    $(".banner-now-bt-list").css("background-color", "#fff");
+    $(".banner-now-bt-list").eq(2).css("background-color", "#006fb2");
+    nowficture = 3;
+  } else if (nowficture == 3) {
+    $(".banner-list").css("transform", "translateX(-60%)");
+    $(".banner-now-bt-list").css("background-color", "#fff");
+    $(".banner-now-bt-list").eq(3).css("background-color", "#006fb2");
+    nowficture = 4;
+  } else if (nowficture == 4) {
+    $(".banner-list").css("transform", "translateX(-80%)");
+    $(".banner-now-bt-list").css("background-color", "#fff");
+    $(".banner-now-bt-list").eq(4).css("background-color", "#006fb2");
+    nowficture = 5;
+  } else if (nowficture == 5) {
+    $(".banner-list").css("transform", "translateX(0)");
+    $(".banner-now-bt-list").css("background-color", "#fff");
+    $(".banner-now-bt-list").eq(0).css("background-color", "#006fb2");
+    nowficture = 1;
+  }
+});
+
+$(".banner-before-bt").on("click", function () {
+  if (nowficture == 1) {
+    $(".banner-list").css("transform", "translateX(-80%)");
+    $(".banner-now-bt-list").css("background-color", "#fff");
+    $(".banner-now-bt-list").eq(4).css("background-color", "#006fb2");
+    nowficture = 5;
+  } else if (nowficture == 2) {
+    $(".banner-list").css("transform", "translateX(0)");
+    $(".banner-now-bt-list").css("background-color", "#fff");
+    $(".banner-now-bt-list").eq(0).css("background-color", "#006fb2");
+    nowficture = 1;
+  } else if (nowficture == 3) {
+    $(".banner-list").css("transform", "translateX(-20%)");
+    $(".banner-now-bt-list").css("background-color", "#fff");
+    $(".banner-now-bt-list").eq(1).css("background-color", "#006fb2");
+    nowficture = 2;
+  } else if (nowficture == 4) {
+    $(".banner-list").css("transform", "translateX(-40%)");
+    $(".banner-now-bt-list").css("background-color", "#fff");
+    $(".banner-now-bt-list").eq(2).css("background-color", "#006fb2");
+    nowficture = 3;
+  } else if (nowficture == 5) {
+    $(".banner-list").css("transform", "translateX(-60%)");
+    $(".banner-now-bt-list").css("background-color", "#fff");
+    $(".banner-now-bt-list").eq(3).css("background-color", "#006fb2");
+    nowficture = 4;
+  }
+});
+
+$(".banner-now-bt-list")
+  .eq(0)
+  .on("click", function () {
+    $(".banner-list").css("transform", "translateX(0)");
+    $(".banner-now-bt-list").css("background-color", "#fff");
+    $(".banner-now-bt-list").eq(0).css("background-color", "#006fb2");
+    nowficture = 1;
+  });
+$(".banner-now-bt-list")
+  .eq(1)
+  .on("click", function () {
+    $(".banner-list").css("transform", "translateX(-20%)");
+    $(".banner-now-bt-list").css("background-color", "#fff");
+    $(".banner-now-bt-list").eq(1).css("background-color", "#006fb2");
+    nowficture = 2;
+  });
+$(".banner-now-bt-list")
+  .eq(2)
+  .on("click", function () {
+    $(".banner-list").css("transform", "translateX(-40%)");
+    $(".banner-now-bt-list").css("background-color", "#fff");
+    $(".banner-now-bt-list").eq(2).css("background-color", "#006fb2");
+    nowficture = 3;
+  });
+$(".banner-now-bt-list")
+  .eq(3)
+  .on("click", function () {
+    $(".banner-list").css("transform", "translateX(-60%)");
+    $(".banner-now-bt-list").css("background-color", "#fff");
+    $(".banner-now-bt-list").eq(3).css("background-color", "#006fb2");
+    nowficture = 4;
+  });
+$(".banner-now-bt-list")
+  .eq(4)
+  .on("click", function () {
+    $(".banner-list").css("transform", "translateX(-80%)");
+    $(".banner-now-bt-list").css("background-color", "#fff");
+    $(".banner-now-bt-list").eq(4).css("background-color", "#006fb2");
+    nowficture = 5;
+  });
