0be49796ea7c6a21e381cbc46ec6501fbb695dec
WonJun-Yeo <rbgh004@gmail.com>
2021-11-30 17:35:11 +0900

    -

--- blizzard/blizzard.js
+++ blizzard/blizzard.js
@@ -3,28 +3,28 @@ var nowficture = 1;
 $(".banner-after-bt").on("click", function () {
   if (nowficture == 1) {
     $(".banner-list").css("transform", "translateX(-20%)");
-    $(".banner-now-bt-list").css("background-color", "#fff");
-    $(".banner-now-bt-list").eq(1).css("background-color", "#006fb2");
+    $(".banner-now-bt-list").css("background-color", "#006fb2");
+    $(".banner-now-bt-list").eq(1).css("background-color", "#00aeff");
     nowficture = 2;
   } else if (nowficture == 2) {
     $(".banner-list").css("transform", "translateX(-40%)");
-    $(".banner-now-bt-list").css("background-color", "#fff");
-    $(".banner-now-bt-list").eq(2).css("background-color", "#006fb2");
+    $(".banner-now-bt-list").css("background-color", "#006fb2");
+    $(".banner-now-bt-list").eq(2).css("background-color", "#00aeff");
     nowficture = 3;
   } else if (nowficture == 3) {
     $(".banner-list").css("transform", "translateX(-60%)");
-    $(".banner-now-bt-list").css("background-color", "#fff");
-    $(".banner-now-bt-list").eq(3).css("background-color", "#006fb2");
+    $(".banner-now-bt-list").css("background-color", "#006fb2");
+    $(".banner-now-bt-list").eq(3).css("background-color", "#00aeff");
     nowficture = 4;
   } else if (nowficture == 4) {
     $(".banner-list").css("transform", "translateX(-80%)");
-    $(".banner-now-bt-list").css("background-color", "#fff");
-    $(".banner-now-bt-list").eq(4).css("background-color", "#006fb2");
+    $(".banner-now-bt-list").css("background-color", "#006fb2");
+    $(".banner-now-bt-list").eq(4).css("background-color", "#00aeff");
     nowficture = 5;
   } else if (nowficture == 5) {
     $(".banner-list").css("transform", "translateX(0)");
-    $(".banner-now-bt-list").css("background-color", "#fff");
-    $(".banner-now-bt-list").eq(0).css("background-color", "#006fb2");
+    $(".banner-now-bt-list").css("background-color", "#006fb2");
+    $(".banner-now-bt-list").eq(0).css("background-color", "#00aeff");
     nowficture = 1;
   }
 });
@@ -32,28 +32,28 @@ $(".banner-after-bt").on("click", function () {
 $(".banner-before-bt").on("click", function () {
   if (nowficture == 1) {
     $(".banner-list").css("transform", "translateX(-80%)");
-    $(".banner-now-bt-list").css("background-color", "#fff");
-    $(".banner-now-bt-list").eq(4).css("background-color", "#006fb2");
+    $(".banner-now-bt-list").css("background-color", "#006fb2");
+    $(".banner-now-bt-list").eq(4).css("background-color", "#00aeff");
     nowficture = 5;
   } else if (nowficture == 2) {
     $(".banner-list").css("transform", "translateX(0)");
-    $(".banner-now-bt-list").css("background-color", "#fff");
-    $(".banner-now-bt-list").eq(0).css("background-color", "#006fb2");
+    $(".banner-now-bt-list").css("background-color", "#006fb2");
+    $(".banner-now-bt-list").eq(0).css("background-color", "#00aeff");
     nowficture = 1;
   } else if (nowficture == 3) {
     $(".banner-list").css("transform", "translateX(-20%)");
-    $(".banner-now-bt-list").css("background-color", "#fff");
-    $(".banner-now-bt-list").eq(1).css("background-color", "#006fb2");
+    $(".banner-now-bt-list").css("background-color", "#006fb2");
+    $(".banner-now-bt-list").eq(1).css("background-color", "#00aeff");
     nowficture = 2;
   } else if (nowficture == 4) {
     $(".banner-list").css("transform", "translateX(-40%)");
-    $(".banner-now-bt-list").css("background-color", "#fff");
-    $(".banner-now-bt-list").eq(2).css("background-color", "#006fb2");
+    $(".banner-now-bt-list").css("background-color", "#006fb2");
+    $(".banner-now-bt-list").eq(2).css("background-color", "#00aeff");
     nowficture = 3;
   } else if (nowficture == 5) {
     $(".banner-list").css("transform", "translateX(-60%)");
-    $(".banner-now-bt-list").css("background-color", "#fff");
-    $(".banner-now-bt-list").eq(3).css("background-color", "#006fb2");
+    $(".banner-now-bt-list").css("background-color", "#006fb2");
+    $(".banner-now-bt-list").eq(3).css("background-color", "#00aeff");
     nowficture = 4;
   }
 });
@@ -62,39 +62,39 @@ $(".banner-now-bt-list")
   .eq(0)
   .on("click", function () {
     $(".banner-list").css("transform", "translateX(0)");
-    $(".banner-now-bt-list").css("background-color", "#fff");
-    $(".banner-now-bt-list").eq(0).css("background-color", "#006fb2");
+    $(".banner-now-bt-list").css("background-color", "#006fb2");
+    $(".banner-now-bt-list").eq(0).css("background-color", "#00aeff");
     nowficture = 1;
   });
 $(".banner-now-bt-list")
   .eq(1)
   .on("click", function () {
     $(".banner-list").css("transform", "translateX(-20%)");
-    $(".banner-now-bt-list").css("background-color", "#fff");
-    $(".banner-now-bt-list").eq(1).css("background-color", "#006fb2");
+    $(".banner-now-bt-list").css("background-color", "#006fb2");
+    $(".banner-now-bt-list").eq(1).css("background-color", "#00aeff");
     nowficture = 2;
   });
 $(".banner-now-bt-list")
   .eq(2)
   .on("click", function () {
     $(".banner-list").css("transform", "translateX(-40%)");
-    $(".banner-now-bt-list").css("background-color", "#fff");
-    $(".banner-now-bt-list").eq(2).css("background-color", "#006fb2");
+    $(".banner-now-bt-list").css("background-color", "#006fb2");
+    $(".banner-now-bt-list").eq(2).css("background-color", "#00aeff");
     nowficture = 3;
   });
 $(".banner-now-bt-list")
   .eq(3)
   .on("click", function () {
     $(".banner-list").css("transform", "translateX(-60%)");
-    $(".banner-now-bt-list").css("background-color", "#fff");
-    $(".banner-now-bt-list").eq(3).css("background-color", "#006fb2");
+    $(".banner-now-bt-list").css("background-color", "#006fb2");
+    $(".banner-now-bt-list").eq(3).css("background-color", "#00aeff");
     nowficture = 4;
   });
 $(".banner-now-bt-list")
   .eq(4)
   .on("click", function () {
     $(".banner-list").css("transform", "translateX(-80%)");
-    $(".banner-now-bt-list").css("background-color", "#fff");
-    $(".banner-now-bt-list").eq(4).css("background-color", "#006fb2");
+    $(".banner-now-bt-list").css("background-color", "#006fb2");
+    $(".banner-now-bt-list").eq(4).css("background-color", "#00aeff");
     nowficture = 5;
   });
